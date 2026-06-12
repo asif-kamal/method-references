3a7c708f61f62e96aa20ccdab0aba30d6e55ad6a
asif-kamal <asif.kamal1489@gmail.com>
2026-06-11 19:36:01 -0500

    list of unary operators of type String as parameter

---
+++ src/main/java/com/asifkamal/MainTwo.java
@@ -0,0 +1,20 @@
+package com.asifkamal;
+
+import java.util.List;
+import java.util.Random;
+import java.util.function.UnaryOperator;
+
+public class MainTwo {
+
+    private static Random random = new Random();
+
+    public static void main(String[] args) {
+        String[] arr = {"HaRRy", "PoTTEr", "Ronald", "weasley", "HERMIONE", "gRAINger"};
+
+
+    }
+
+    private static void applyChanges(String[] names, List<UnaryOperator<String>> changes) {
+
+    }
+}
